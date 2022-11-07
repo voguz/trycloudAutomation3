6022315eb1133c0218e27c37354e60c6125a0873
Volkan Oguz <oquzvolkan@gmail.com>
2022-11-07 17:36:27 -0600

    MyUserStory feature file updated.

---
+++ src/test/java/com/trycloud/pages/VolkanUS2_PAGE.java
@@ -0,0 +1,24 @@
+package com.trycloud.pages;
+
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class VolkanUS2_PAGE {
+
+    public VolkanUS2_PAGE(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy(id = "user")
+    public WebElement userName;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(id = "submit-form")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//p[@class='warning wrongPasswordMsg']")
+    public WebElement messageDisplayed;
+}
---
+++ src/test/java/com/trycloud/step_definitions/VolkanUS2_StepDef.java
@@ -0,0 +1,42 @@
+package com.trycloud.step_definitions;
+
+import com.github.javafaker.Faker;
+import com.trycloud.pages.VolkanUS2_PAGE;
+import com.trycloud.utilities.ConfigReader;
+import com.trycloud.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class VolkanUS2_StepDef {
+
+    VolkanUS2_PAGE volkan = new VolkanUS2_PAGE();
+    Faker faker=new Faker();
+
+    @Given("user on the login page")
+    public void user_on_the_login_page() {
+        Driver.getDriver().get(ConfigReader.getProperty("env"));
+
+
+    }
+    @When("user enter invalid {string} and {string}")
+    public void user_enter_invalid_and(String string, String string2) {
+        volkan.userName.sendKeys(faker.name().username());
+        volkan.password.sendKeys(faker.bothify("##?#???#?#"));
+
+    }
+    @When("user click the login button")
+    public void user_click_the_login_button() {
+        volkan.loginButton.click();
+
+    }
+    @Then("verify {string} message should be displayed")
+    public void verify_message_should_be_displayed(String string) {
+
+        String ActualMessage = volkan.messageDisplayed.getText();
+
+        Assert.assertEquals("Message Verification",string,ActualMessage);
+
+    }
+}
